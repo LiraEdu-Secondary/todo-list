37d2306d903e19c682e3be56ce60dd3daa75386c
Eduardo Lira <eduardoliracunha17@gmail.com>
2026-09-07 20:43:48 -0400

    feat: adiciona funcionalidade de excluir e concluir tarefas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 "use client"
-
 import Todo from "@/components/todo";
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import { Input } from "@base-ui/react"
-import { ListTodo, Check, Plus, Trash } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { useState } from "react";
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
 const Home = () => {
   const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   // Adiciona nova tarefa
   const addTask = () => {
@@ -17,36 +23,46 @@ const Home = () => {
 
     if (!task) return;
 
-    setTasks([...tasks, task]);
+    setTasks([...tasks, {
+      id: crypto.randomUUID(),
+      title: task,
+      completed: false
+    }]);
 
     setNewTask("");
+  };
+
+  // Remove tarefa
+  const removeTask = (idToRemove: string) => {
+    setTasks(tasks.filter((task) => task.id !== idToRemove));
+  };
+
+
+  const toggleTaskCompleted = (idToToggle: string) => {
+    setTasks(tasks.map((task) => task.id === idToToggle ? { ...task, completed: !task.completed } : task));
   }
-
-
-
 
 
   return (
     <main className="flex min-h-screen items-center justify-center p-4">
       <Card className="flex w-full max-w-md flex-col items-center gap-4">
         <CardContent className="flex w-full flex-row items-center gap-2">
-          {/* <ListTodo className="h-6 w-6" /> */}
           <Input
             className="min-w-0 flex-1 rounded-md border border-gray-300 p-2 outline-none focus:border-gray-500 focus:ring-2 focus:ring-gray-200"
             value={newTask}
-            onChange={(e) => {
-              setNewTask(e.target.value)
-            }}
+            onChange={(e) => setNewTask(e.target.value)}
             placeholder="Crie sua tarefa."
           />
-        <Button className="cursor-pointer" onClick={addTask}><Plus className="h-6 w-6" /></Button>
+          <Button className="cursor-pointer" onClick={addTask}>
+            <Plus className="h-6 w-6" />
+          </Button>
         </CardContent>
-        <Separator className="w-100! " />
+        <Separator className="w-100!" />
 
-        <Todo tasks={tasks}/>
+        <Todo tasks={tasks} onRemoveTask={removeTask} onToggleTaskCompleted={toggleTaskCompleted} />
       </Card>
     </main>
-  )
+  );
 }
 
 export default Home
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -2,23 +2,35 @@ import { Check, Trash } from 'lucide-react'
 import { Card } from './ui/card'
 import { Button } from './ui/button'
 
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
 type TodoProps = {
-  tasks: string[]
+  tasks: Task[]
+  onRemoveTask?: (id: string) => void;
+  onToggleTaskCompleted: (id: string) => void;
 }
 
-const Todo = ({ tasks }: TodoProps) => {
+const Todo = ({ tasks, onRemoveTask, onToggleTaskCompleted }: TodoProps) => {
   return (
     <div className='flex flex-col gap-2'>
       {
         tasks.map((task, index) => (
           <Card key={index} className='flex w-[400] h-[50] flex-col items-start justify-center gap-2 rounded-md border border-gray-300 p-2'>
             <div className="flex w-full items-center p-2">
-              <span className="min-w-0 flex-1 truncate">
-                {task}
+              <span className={`min-w-0 flex-1 truncate ${task.completed ? "line-through text-gray-400" : ""}`}>
+                {task.title}
               </span>
               <div className="ml-auto flex gap-2">
-                <Button size="sm" className="cursor-pointer"><Trash /></Button>
-                <Button size="sm" variant="ghost" className="cursor-pointer"><Check /></Button>
+                <Button size="sm" className="cursor-pointer" onClick={() => onRemoveTask?.(task.id)}>
+                  <Trash />
+                </Button>
+                <Button size="sm" variant="ghost" className="cursor-pointer" onClick={() => onToggleTaskCompleted(task.id)}>
+                  <Check />
+                </Button>
               </div>
             </div>
           </Card>
